e195ab7f17dca5d541e3b1d4c2c75f43dd24333d
Hafijur Rahman <hafijurrahman@Hafijurs-MacBook-Air.local>
2026-01-28 13:55:12 +0600

    compare array and string

--- multiplication.js
+++ multiplication.js
@@ -1,5 +1,5 @@
 const num = 5;
-console.log(`Multiplication of ${num}`)
+console.log("Multiplication of",num)
 
 for (let i = 1; i <= 10; i++){
     const mul = num*i;
---
+++ string.js
@@ -0,0 +1,14 @@
+const array = [23, 56, 67, 78, 56];
+console.log(array);
+console.log(array.length);
+array[1] = 22;
+console.log(array);     //mutable ---> changeable
+
+let string = 'Dhaka';
+console.log(string);
+console.log(string.length);
+string[0] = 'F';
+console.log(string);    //immutable ----> not changeable
+
+
+
